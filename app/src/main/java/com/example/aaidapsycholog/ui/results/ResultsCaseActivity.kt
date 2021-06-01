4e98660e7f7334f7388c7498be838ca6ac1b28fe
Debtya <debtya18ti@mahasiswa.pcr.ac.id>
2021-06-02 00:20:06 +0700

    Update UI
    activity nambah 1

---
+++ app/src/main/java/com/example/aaidapsycholog/ui/results/ResultsCaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aaidapsycholog.ui.results
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aaidapsycholog.R
+
+class ResultsCaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_results)
+    }
+}
